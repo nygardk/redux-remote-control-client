0e13907f32fe41d2ec10e88be69f0a205302f4fc
Klaus Nygård <klaus.nygard@gmail.com>
2016-07-03 14:53:11 +0300

    initial middleware POC

--- demos/demo0/configureStore.js
+++ demos/demo0/configureStore.js
@@ -1,7 +1,8 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { syncMiddleware } from 'redux-remote-control-client';
 
 import rootReducer from './reducers';
 
 export default function configureStore() {
-  return createStore(rootReducer);
+  return createStore(rootReducer, applyMiddleware(syncMiddleware()));
 }
--- src/redux-remote-control-client.js
+++ src/redux-remote-control-client.js
@@ -1,1 +1,62 @@
-// WIP
+const DEFAULT_OPTIONS = {
+  host: 'localhost',
+  port: '8888',
+};
+
+function throttle(delayMs, action) {
+  let wait;
+
+  return data => {
+    if (!wait) {
+      action(data);
+
+      wait = setTimeout(() => {
+        wait = undefined;
+      }, delayMs);
+    }
+  };
+}
+
+function mouseEvent(name, e) {
+  return `${name}_${e.clientX},${e.clientY}`;
+}
+
+export function syncMiddleware(opts) {
+  const options = { ...DEFAULT_OPTIONS, ...opts };
+
+  return store => {
+    const ws = new WebSocket(`ws://${options.host}:${options.port}`);
+
+    ws.onopen = () => {
+      window.onmousemove = throttle(50, e => {
+        ws.send(mouseEvent('WMP', e));
+      });
+
+      window.onmousedown = e => {
+        ws.send(mouseEvent('WMD', e));
+      };
+
+      window.onmouseup = e => {
+        ws.send(mouseEvent('WMU', e));
+      };
+
+      ws.send(JSON.stringify({
+        action: 'SYNC_STATE',
+        data: store.getState(),
+      }));
+    };
+
+    return next => action => {
+      ws.send(JSON.stringify({
+        action: 'ACTION',
+        data: action,
+      }));
+
+      next(action);
+    };
+  };
+}
+
+export function rcWrapper(app) {
+  return app;
+}
